540952504d524ac620087b3e319e17ee096e5e14
Nash Rojas <nash_rojas@ucne.edu.do>
2026-02-04 17:09:07 -0400

    Actualizacion Ejercicios 4 Feb

---
+++ Ejercicios 4-Feb/Empleado.java
@@ -0,0 +1,31 @@
+class Empleado {
+    public double calcularSalario() {
+        return 0;
+    }
+}
+
+class EmpleadoFijo extends Empleado {
+    double salario;
+
+    public EmpleadoFijo(double salario) {
+        this.salario = salario;
+    }
+
+    public double calcularSalario() {
+        return salario;
+    }
+}
+
+class EmpleadoPorHora extends Empleado {
+    int horas;
+    double pagoHora;
+
+    public EmpleadoPorHora(int horas, double pagoHora) {
+        this.horas = horas;
+        this.pagoHora = pagoHora;
+    }
+
+    public double calcularSalario() {
+        return horas * pagoHora;
+    }
+}
